ee72f912e2bb3122d7afe0691a6f6d0047a44b88
timunsted <tim@baileysolutions.co.uk>
2016-11-04 17:11:43 +0000

    Aded check for http:// when adding a useful link

--- slls/ViewModels/UsefulLinksViewModels.cs
+++ slls/ViewModels/UsefulLinksViewModels.cs
@@ -8,6 +8,8 @@ namespace slls.ViewModels
         [Key]
         public int LinkID { get; set; }
 
+        [Required]
+        [RegularExpression(@"^(http(s)?://|ftp://|file://|[\w]\:|\\).+$", ErrorMessage = "This URL does not appear to be valid. Are you missing a prefix (e.g. 'http://')?")]
         [LocalDisplayName("Useful_links.Link_Address", "FieldDisplayName")]
         public string LinkAddress { get; set; }
 
