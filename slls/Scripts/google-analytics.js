fc3225546c314c84412c8891801fd0e1056ad57e
timunsted <tim@baileysolutions.co.uk>
2016-11-08 16:29:08 +0000

    Added appSetting for Google Analytics tracking ID

--- slls/Scripts/google-analytics.js
+++ slls/Scripts/google-analytics.js
@@ -6,5 +6,5 @@
     m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m);
 })(window, document, "script", "https://www.google-analytics.com/analytics.js", "ga");
 
-ga("create", "UA-86654061-1", "auto");
+ga("create", gaTrackId, "auto");
 ga("send", "pageview");
